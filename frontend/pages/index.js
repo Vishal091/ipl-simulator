336a2630a28d8b3472f234aa2587721aa0c9b01e
Vishal091 <vishalvats09@gmail.com>
2026-03-20 20:21:02 +0530

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,3 @@
-const simulateMatch = async () => {
-  const res = await fetch("https://ipl-simulator-tb8n.onrender.com/simulate", {
-    method: "POST"
-  });
-
-  const data = await res.json();
-  setResult(data);
-};
+export default function Home() {
+  return <h1>WORKING AGAIN 🚀</h1>;
+}
