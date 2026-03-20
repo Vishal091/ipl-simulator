dc491b24108abb3842465533953cb1194234fa60
Vishal091 <vishalvats09@gmail.com>
2026-03-20 19:47:28 +0530

    Update index.js

--- frontend/ pages/index.js
+++ frontend/ pages/index.js
@@ -1,33 +1,7 @@
-import { useState } from "react";
-
 export default function Home() {
-  const [result, setResult] = useState(null);
-
-  const simulateMatch = async () => {
-    const res = await fetch("https://ipl-simulator-tb8n.onrender.com/simulate", {
-      method: "POST"
-    });
-
-    const data = await res.json();
-    setResult(data);
-  };
-
   return (
-    <div style={{ padding: 20 }}>
-      <h1>IPL Simulator 🏏</h1>
-
-      <button onClick={simulateMatch}>
-        Simulate Match
-      </button>
-
-      {result && (
-        <div>
-          <h2>Result</h2>
-          <p>{result.team1}: {result.score1}</p>
-          <p>{result.team2}: {result.score2}</p>
-          <p><b>Winner: {result.winner}</b></p>
-        </div>
-      )}
+    <div>
+      <h1>IPL Simulator Working 🚀</h1>
     </div>
   );
 }
