108cfe4e806800449dfdf0107ed23b17f52fc27d
Vishal091 <vishalvats09@gmail.com>
2026-03-20 20:26:47 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 import random
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def home():
     return {"message": "IPL Simulator API Running"}
