95308094d25e07795ef2f526748b9e1a25000310
Vishal091 <vishalvats09@gmail.com>
2026-03-21 20:52:28 +0530

    Create layout.js

---
+++ frontend/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "IPL Simulator",
+  description: "Cricket Simulation Game"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
